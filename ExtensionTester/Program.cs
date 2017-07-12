37134bc4a4d9a6651db71bf9f463a9711a46ad92
Mary <mshultz@student.neumont.edu>
2017-07-12 00:17:01 -0600

    Initial Commit of Extension method project

---
+++ ExtensionTester/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShultzM_ExtensionMethods;
+
+namespace ExtensionTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestPrintWithInt();
+            TestPrintWithString();
+            Console.WriteLine();
+            TestToPower();
+            Console.WriteLine();
+            TestIsPalindrome();
+        }
+
+        private static void TestPrintWithInt()
+        {
+            List<int> testers = new List<int>
+            {
+                1, 2, 3, 4
+            };
+            Console.WriteLine("-----Print Method Test Int------");
+            Console.WriteLine("Expected: 1, 2, 3, 4");
+            testers.Print();
+            PrintLineBreak();
+
+        }
+
+        private static void TestPrintWithString()
+        {
+            List<String> testers = new List<String>
+            {
+                "Apple", "Banana", "Cucumber", "D-food"
+            };
+            Console.WriteLine("-----Print Method Test String-----");
+            Console.WriteLine("Expected:  Apple, Banana, Cucumber, D-food");
+            testers.Print();
+        }
+
+        private static void TestToPower()
+        {
+            Console.WriteLine("-----To Power Test-----");
+            Console.WriteLine("Testing 2^2");
+            Console.WriteLine("Expected: {0}", 4);
+            Console.WriteLine("Tested: {0}", 2.ToPower(2));
+            PrintLineBreak();
+
+            Console.WriteLine("Testing 150^3");
+            Console.WriteLine("Expected: {0}", 3375000);
+            Console.WriteLine("Tested: {0}", 150.ToPower(3));
+            PrintLineBreak();
+
+            Console.WriteLine("Testing 2^16");
+            Console.WriteLine("Expected: {0}", 65536);
+            Console.WriteLine("Tested: {0}", 2.ToPower(16));
+        }
+
+        private static void TestIsPalindrome()
+        {
+            Console.WriteLine("-----Is Palindrome Test-----");
+
+            String testString = "Bob";
+            Console.WriteLine("Testing \"{0}\"", testString);
+            Console.WriteLine("Expected: {0}", true);
+            Console.WriteLine("Tested: {0}", testString.IsPalindrome());
+            PrintLineBreak();
+
+            testString = "Taco cat";
+            Console.WriteLine("Testing \"{0}\"", testString);
+            Console.WriteLine("Expected: {0}", true);
+            Console.WriteLine("Tested: {0}", testString.IsPalindrome());
+            PrintLineBreak();
+
+            testString = "Taco cat";
+            Console.WriteLine("Testing \"{0}\"", testString);
+            Console.WriteLine("Expected: {0}", true);
+            Console.WriteLine("Tested: {0}", testString.IsPalindrome());
+            PrintLineBreak();
+
+            testString = "TAcoCat";
+            Console.WriteLine("Testing \"{0}\"", testString);
+            Console.WriteLine("Expected: {0}", true);
+            Console.WriteLine("Tested: {0}", testString.IsPalindrome());
+            PrintLineBreak();
+
+            testString = "raceCar";
+            Console.WriteLine("Testing \"{0}\"", testString);
+            Console.WriteLine("Expected: {0}", true);
+            Console.WriteLine("Tested: {0}", testString.IsPalindrome());
+
+            PrintLineBreak();
+            testString = "Cuppycakes";
+            Console.WriteLine("Testing \"{0}\"", testString);
+            Console.WriteLine("Expected: {0}", false);
+            Console.WriteLine("Tested: {0}", testString.IsPalindrome());
+        }
+
+        private static void PrintLineBreak()
+        {
+            Console.WriteLine("-----------------------");
+        }
+    }
+}
---
+++ ShultzM_ExtensionMethods/Extensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShultzM_ExtensionMethods
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Prints out the objects, separating each value with a comma (,) while making sure there is NO dangling tail symbol.
+        /// </summary>
+        /// <typeparam name="T">The element type of the collection</typeparam>
+        /// <param name="items">A collection of objects</param>
+        public static void Print<T>(this IEnumerable<T> items)
+        {
+            String printable = "";
+            foreach(T item in items){
+                printable += item.ToString() + ", ";
+            }
+            Console.WriteLine(printable.TrimEnd(' ').TrimEnd(','));
+        }
+
+        /// <summary>
+        /// Performs a power calculation on the provided int
+        /// </summary>
+        /// <param name="baseNum"></param>
+        /// <param name="exponent"></param>
+        /// <returns>The calculated </returns>
+        public static int ToPower(this int baseNum, int exponent)
+        {
+            return (int) Math.Pow((double)baseNum, (double)exponent);
+        }
+
+        /// <summary>
+        /// Returns a bool signifying whether the string is a palindrome (reads backward the same as forward), ignoring both casing and any whitespace.
+        /// </summary>
+        /// <param name="testingString">String to be tested</param>
+        /// <returns>A bool signifying whether the string is a palindrome</returns>
+        public static bool IsPalindrome(this String testingString)
+        {
+           String tester = testingString.Replace(" ", "").ToLower();
+            return tester.SequenceEqual(tester.Reverse());
+
+        }
+
+        /// <summary>
+        /// Determines whether two string equal one another, no matter the case.
+        /// </summary>
+        /// <param name="initialString">The string to be tested against</param>
+        /// <param name="testingString">The string to test</param>
+        /// <returns>A bool indicating whether they equal or not</returns>
+        public static bool EqualsIgnoreCase(this string initialString, string testingString)
+        {
+            return initialString.Equals(testingString, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
